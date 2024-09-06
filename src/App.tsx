43c578d40536cdc4dcb389d6d18ff5cfd842040b
Ullrich <tobias.ullrich@FKIE>
2024-09-06 19:28:23 +0200

    added leader board

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,105 @@
 import { useState } from "react";
-import "./App.css";
+import "./assets/css/App.css";
 import "./assets/css/Tile.css";
 import "./assets/css/Board.css";
+import "./assets/css/LeaderBoard.css";
 import { Board } from "./components/UI/Board";
+import { LeaderBoard } from "./components/LeaderBoard";
 
 function App() {
   // current symbol über props runtergeben
+
   const [valueOfTiles, setValueOfTiles] = useState<("X" | "O" | undefined)[]>(
     Array(9).fill(undefined)
   );
   const [currentSymbol, setCurrentSymbol] = useState<"X" | "O">("X");
   const [winner, setWinner] = useState<"X" | "O" | undefined>(undefined);
+  const [numberOfWins, setNumberOfWins] = useState<{ X: number; O: number }>({
+    X: 0,
+    O: 0,
+  });
 
   const checkForWinner = (valueOfTiles: ("X" | "O" | undefined)[]) => {
     // Check for winner in column
-    for (let i = 0; i <= valueOfTiles.length - 6; i++) {
-      if (
-        valueOfTiles[i] === valueOfTiles[i + 3] &&
-        valueOfTiles[i] === valueOfTiles[i + 6] &&
-        valueOfTiles[i] != undefined
-      ) {
-        setWinner(valueOfTiles[i]);
-        return;
-      }
-    }
-    // Check for winner in row
-    for (let i = 0; i < valueOfTiles.length - 3; i += 3) {
+    for (let i = 0; i <= valueOfTiles.length - 2; i += 3) {
+      console.log(valueOfTiles);
       if (
         valueOfTiles[i] === valueOfTiles[i + 1] &&
         valueOfTiles[i] === valueOfTiles[i + 2] &&
         valueOfTiles[i] != undefined
       ) {
         setWinner(valueOfTiles[i]);
+        setNumberOfWins((prevState) => ({
+          ...prevState,
+          [valueOfTiles[i] as "X" | "O"]:
+            prevState[valueOfTiles[i] as "X" | "O"] + 1,
+        }));
+
+        return;
+      }
+    }
+    // Check for winner in row
+    for (let i = 0; i < valueOfTiles.length - 6; i++) {
+      if (
+        valueOfTiles[i] === valueOfTiles[i + 3] &&
+        valueOfTiles[i] === valueOfTiles[i + 6] &&
+        valueOfTiles[i] != undefined
+      ) {
+        setWinner(valueOfTiles[i]);
+
+        setNumberOfWins((prevState) => ({
+          ...prevState,
+          [valueOfTiles[i] as "X" | "O"]:
+            prevState[valueOfTiles[i] as "X" | "O"] + 1,
+        }));
+
         return;
       }
     }
     // Check for winner in diagonal
-    if(valueOfTiles[0] === valueOfTiles[4] && valueOfTiles[0] === valueOfTiles[8] && valueOfTiles[0] != undefined){
-      setWinner(valueOfTiles[0])
-      return
+    if (
+      valueOfTiles[0] === valueOfTiles[4] &&
+      valueOfTiles[0] === valueOfTiles[8] &&
+      valueOfTiles[0] != undefined
+    ) {
+      setWinner(valueOfTiles[0]);
+      setNumberOfWins((prevState) => ({
+        ...prevState,
+        [valueOfTiles[0] as "X" | "O"]:
+          prevState[valueOfTiles[0] as "X" | "O"] + 1,
+      }));
+
+      return;
     }
-    if(valueOfTiles[6] === valueOfTiles[4] && valueOfTiles[6] === valueOfTiles[2] && valueOfTiles[6] != undefined){
-      setWinner(valueOfTiles[6])
-      return
+    if (
+      valueOfTiles[6] === valueOfTiles[4] &&
+      valueOfTiles[6] === valueOfTiles[2] &&
+      valueOfTiles[6] != undefined
+    ) {
+      setWinner(valueOfTiles[6]);
+
+      setNumberOfWins((prevState) => ({
+        ...prevState,
+        [valueOfTiles[6] as "X" | "O"]:
+          prevState[valueOfTiles[6] as "X" | "O"] + 1,
+      }));
+
+      return;
     }
   };
 
-const checkForFullBoard = (valueOfTiles: ("X" | "O" | undefined)[]) => {
-  for(let i = 0; i < valueOfTiles.length; i++){
-    if (valueOfTiles[i] === undefined){
-      return false
+  const checkForFullBoard = (valueOfTiles: ("X" | "O" | undefined)[]) => {
+    for (let i = 0; i < valueOfTiles.length; i++) {
+      if (valueOfTiles[i] === undefined) {
+        return false;
+      }
     }
-  }
-  return true
-}
+    return true;
+  };
 
   const handleTileClick = (index: number) => {
-    if (valueOfTiles[index] === undefined && winner === undefined) { // tile not yet occupied and no winner yet
+    if (valueOfTiles[index] === undefined && winner === undefined) {
+      // tile not yet occupied and no winner yet
       const newValueOfTiles = [...valueOfTiles];
       newValueOfTiles[index] = currentSymbol;
       setValueOfTiles(newValueOfTiles);
@@ -66,18 +109,46 @@ const checkForFullBoard = (valueOfTiles: ("X" | "O" | undefined)[]) => {
   };
 
   const handleNewGame = () => {
-    setValueOfTiles(Array(9).fill(undefined))
-    setWinner(undefined)
-    setCurrentSymbol("X")
-  }
+    setValueOfTiles(Array(9).fill(undefined));
+    setWinner(undefined);
+    setCurrentSymbol("X");
+  };
 
   return (
     <div className="appContainer">
-      <h1>Tic Tac Toe</h1>
-      <Board onTileClick={handleTileClick} valueOfTiles={valueOfTiles} currentSymbol = {currentSymbol} />
-      <h3>{checkForFullBoard(valueOfTiles) ? "The Board is full" : winner ? `The winner is: ${winner}` : `Player ${currentSymbol}`}</h3>
-      <button className={!winner && !checkForFullBoard(valueOfTiles) ? "hiddenButton" : "playAgainButton"} onClick={handleNewGame}>Play Again</button> 
+      
+    <div className="leaderBoardWrapper">
+    <LeaderBoard numberOfWins={numberOfWins} />
     </div>
+ 
+      
+      
+      <h1>Tic Tac Toe</h1>
+      <Board
+        onTileClick={handleTileClick}
+        valueOfTiles={valueOfTiles}
+        currentSymbol={currentSymbol}
+      />
+      <h3>
+        {checkForFullBoard(valueOfTiles)
+          ? "The Board is full"
+          : winner
+          ? `The winner is: ${winner}`
+          : `Player ${currentSymbol}`}
+      </h3>
+      <button
+        className={
+          !winner && !checkForFullBoard(valueOfTiles)
+            ? "hiddenButton"
+            : "playAgainButton"
+        }
+        onClick={handleNewGame}
+      >
+        Play Again
+      </button>
+      </div>
+      
+ 
   );
 }
 
---
+++ src/components/LeaderBoard.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface LeaderBoardProps {
+  numberOfWins: { "X": number; "O": number };
+}
+
+export const LeaderBoard: React.FC<LeaderBoardProps> = ({ numberOfWins }) => {
+  return <div className="leaderBoardContainer">
+    <div className="playerX">
+    
+    {`Player X - ${numberOfWins.X} wins`} 
+    </div>
+    <div className="playerY">
+    {`Player O - ${numberOfWins.O} wins`} 
+    </div>
+  </div>;
+};
